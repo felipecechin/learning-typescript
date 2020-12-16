22daa27fbee35cc5261d0e7452370b2929de7c7f
Felipe <ficechin@hotmail.com>
2020-12-16 16:49:45 -0300

    Section 5 - lesson 71.

--- classes/classes.ts
+++ classes/classes.ts
@@ -57,3 +57,41 @@ console.log(prod1.resumo())
 const prod2 = new Produto('opa', 4.20, 0.06)
 console.log(prod2.resumo())
 console.log(prod2.precoComDesconto())
+
+class Carro {
+    private velocidadeAtual: number = 0
+
+    constructor(public marca: string, public modelo: string, private velocidadeMaxima: number = 200) {
+
+    }
+
+    private alterarVelocidade(delta: number): number {
+        const novaVelocidade = this.velocidadeAtual + delta
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade
+        } else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+        return this.velocidadeAtual
+    }
+
+    public acelerar(): number {
+        return this.alterarVelocidade(5)
+    }
+
+    public frear(): number {
+        return this.alterarVelocidade(-5)
+    }
+}
+
+const carro1 = new Carro('Ford', 'Ka', 175)
+Array(50).fill(0).forEach(()=>carro1.acelerar())
+console.log(carro1.acelerar())
+
+Array(20).fill(0).forEach(()=>carro1.frear())
+console.log(carro1.frear())
+
+//simular "erros"
+// carro1.velocidadeAtual = 300
+// console.log('atual -> '+carro1.velocidadeAtual)
